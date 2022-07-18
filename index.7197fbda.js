4efdb5e74a49957fe134c97aa57f058e30d6eb3e
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-07-18 17:14:30 +0000

    Deploying to gh-pages from @ Demetryj/ice-cream-hk@1935da960c8ee62d297cee65a2cd39873607779c 🚀

--- index.7197fbda.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll"),n}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.7197fbda.js.map
